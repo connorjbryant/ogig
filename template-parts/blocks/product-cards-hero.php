ab0ce212d8836f1add22ffb33fe70ba22e00946d
Connor Bryant <connorjamesbryant7@gmail.com>
2026-02-18 15:48:27 -0500

    Hero update

--- template-parts/blocks/product-cards-hero.php
+++ template-parts/blocks/product-cards-hero.php
@@ -68,7 +68,7 @@ $active_count = count($active_cards);
 
   <div class="ogig-pch__inner site-container">
     <?php if (!empty($heading)) : ?>
-      <h1 class="ogig-pch__title" data-aos="fade-down"><?php echo esc_html($heading); ?></h1>
+      <h1 class="ogig-pch__title custom-align" data-aos="fade-down"><?php echo esc_html($heading); ?></h1>
     <?php endif; ?>
 
     <?php if ($active_count > 0) : ?>
